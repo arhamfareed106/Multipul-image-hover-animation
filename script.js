b55799a044cb978fbae92f4f558b132bb5258044
arhamfareed106 <arhamfareed575@gmail.com>
2024-09-15 00:00:43 +0500

    correct the code

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var elem = document.querySelectorAll('.elem');
 
-elem.forEach(function(val){
-val.addEventListener("mouseenter", function(){
-    val.style.background= "red"
-})
-val.addEventListener("mouseleave", function(){
-    val.style.background= "transparent"
-})
-})
+elem.forEach(function(val) {
+
+    val.addEventListener("mouseenter", function() {
+        // Use querySelector to target the img element directly
+        var img = val.querySelector('img');
+        img.style.opacity = 1;
+    });
+
+    val.addEventListener("mouseleave", function() {
+        var img = val.querySelector('img');
+        img.style.opacity = 0;
+    });
+
+    val.addEventListener("mousemove", function(dets) {
+        var img = val.querySelector('img');
+        // Use clientX and clientY for mouse coordinates
+        img.style.left = dets.clientX + "px";
+        img.style.top = dets.clientY + "px";
+    });
+
+});
